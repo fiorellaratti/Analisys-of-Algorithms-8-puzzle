8791d8dd5f6dff45bf7746536ada6b1a76bb386b
fiorellaratti <110804082+fiorellaratti@users.noreply.github.com>
2023-04-03 13:42:55 -0400

    BFS algorithm with main function to test

--- BFS.py
+++ BFS.py
@@ -1,22 +1,66 @@
-import queue
+
+from queue import Queue
 
 def Breadth_First_Search(initial, goal):
-    q = queue.Queue()
-    visited = []
-    cost = 0
+    moves = ['up', 'down', 'left', 'right']
 
-    if initial == goal:
-        return 0
-    else: 
-        while (q.length):
-            q.get()
+    initial = tuple(initial)
+    goal = tuple(goal)
+
+    visited = set()
+    q = Queue()
+    q.put((initial, 0))
+
+    while not q.empty():
+        curr_state, cost = q.get()
+        
+        if curr_state == goal:
+            return cost
+        
+        visited.add(curr_state)
+        
+        zero_index = curr_state.index(0)
+        
+        zero_row, zero_col = divmod(zero_index, 3)
+
+        for move in moves:
+            if move == 'up':
+                new_row = zero_row - 1
+                new_col = zero_col
+            elif move == 'down':
+                new_row = zero_row + 1
+                new_col = zero_col
+            elif move == 'left':
+                new_row = zero_row
+                new_col = zero_col - 1
+            elif move == 'right':
+                new_row = zero_row
+                new_col = zero_col + 1
             
+            if 0 <= new_row < 3:
+                if 0 <= new_col < 3:
+                    new_index = new_row * 3 + new_col
+                    
+                    new_state = list(curr_state)
+                    new_state[zero_index], new_state[new_index] = new_state[new_index], new_state[zero_index]
+                    new_state = tuple(new_state)
+                    
+                    if new_state not in visited:
+                        q.put((new_state, cost + 1)) 
+            else:
+                continue
+    return "Unsolvable"
+
 
 def main():
-    initial = [1, 2, 3, 8, 0, 4, 7, 6, 5]
-    goal = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+    initial = [1, 3, 4, 8, 0, 2, 7, 6, 5]
+    goal = [1, 2, 3, 8, 0 ,4, 7, 6, 5]
 
+    cost = Breadth_First_Search(initial, goal)
 
-    print(Breadth_First_Search(initial, goal))
+    if cost == "Unsolvable":
+        print("Goal state not found")
+    else:
+        print("Final cost to reach goal state:", cost)
 
 main()
