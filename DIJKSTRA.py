af1e1e02cd8a4a1d03bb687ed7c4201eb9e677d4
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-04-06 11:28:35 -0400

    Update DIJKSTRA.py

--- DIJKSTRA.py
+++ DIJKSTRA.py
@@ -1,60 +1,79 @@
-from queue import PriorityQueue
+import heapq  # Importing the heapq module for implementing priority queue.
 
 def Dijkstra_Search(initial, goal):
-    moves = ['up', 'down', 'left', 'right']
+    moves = ['up', 'down', 'left', 'right']  # List of all possible moves.
 
-    # Set to keep track of visited states
-    visited = set()
+    # Converting the given initial and goal state lists to tuples for hashing.
+    initial = tuple(initial)
+    goal = tuple(goal)
 
-    # Priority queue to store states to be visited, with the cost as the priority
-    pq = PriorityQueue()
-    pq.put((0, initial)) # Add the initial state with a cost of 0
+    heap = [(0, initial)]  # Initializing a priority queue with initial state and priority 0.
+    cost_so_far = {initial: 0}  # Initializing a dictionary to store the cost of each state.
 
-    while not pq.empty():
-        cost, curr_state = pq.get() # Get the state with the lowest cost from the priority queue
-        
-        if curr_state == goal: # If the current state is the goal state, return the cost
-            return cost
-        
-        visited.add(curr_state) # Add the current state to the visited set
-        
-        zero_index = curr_state.index(0) # Find the index of the empty tile
-        
-        zero_row, zero_col = divmod(zero_index, 3) # Calculate the row and column of the empty tile
+    while heap:
+        _, curr_state = heapq.heappop(heap)  # Getting the state with minimum cost from the priority queue.
 
-        # Loop through all possible moves
-        for move in moves:
-            if move == 'up': # If moving the empty tile up
+        if curr_state == goal:  # If the current state is the goal state, return the cost to reach this state.
+            return cost_so_far[curr_state]
+
+        zero_index = curr_state.index(0)  # Finding the index of the empty tile (0).
+
+        zero_row, zero_col = divmod(zero_index, 3)  # Finding the row and column of the empty tile.
+
+        for move in moves:  # Looping through all possible moves.
+            if move == 'up':
                 new_row = zero_row - 1
                 new_col = zero_col
-            elif move == 'down': # If moving the empty tile down
+            elif move == 'down':
                 new_row = zero_row + 1
                 new_col = zero_col
-            elif move == 'left': # If moving the empty tile left
+            elif move == 'left':
                 new_row = zero_row
                 new_col = zero_col - 1
-            elif move == 'right': # If moving the empty tile right
+            elif move == 'right':
                 new_row = zero_row
                 new_col = zero_col + 1
-            
-            # Check if the new row and column are within bounds of the puzzle
-            if 0 <= new_row < 3 and 0 <= new_col < 3:
-                new_index = new_row * 3 + new_col # Calculate the index of the tile to be swapped with the empty tile
-                
-                new_state = list(curr_state) # Create a new list to store the new state
-                value = new_state[new_index] # Get the value of the tile being swapped
-                new_state[zero_index], new_state[new_index] = new_state[new_index], new_state[zero_index] # Swap the tiles
-                new_state = tuple(new_state) # Convert the new state back to a tuple
-                
-                if new_state not in visited: # If the new state has not been visited before
-                    pq.put((cost + value, new_state)) # Add the new state with the updated cost to the priority queue 
-            else:
-                continue # If the new row or column is out of bounds, skip this move
-    return "Unsolvable" # If the goal state is not reached, return "Unsolvable"
 
-initial = (7, 2, 4, 5, 0, 6, 8, 3, 1)
-goal1 = (1, 3, 4, 8, 0 ,2, 7, 6, 5)
-goal2 = (1, 3, 4, 8, 0, 6, 7, 5, 2)
+            if 0 <= new_row < 3:  # If the new row is within the bounds of the puzzle.
+                if 0 <= new_col < 3:  # If the new column is within the bounds of the puzzle.
+                    new_index = new_row * 3 + new_col  # Finding the index of the tile to be swapped with the empty tile.
+
+                    new_state = list(curr_state)  # Creating a new state by swapping the empty tile with the tile to be moved.
+                    new_state[zero_index], new_state[new_index] = new_state[new_index], new_state[zero_index]
+                    new_state = tuple(new_state)  # Converting the new state to a tuple.
+
+                    new_cost = cost_so_far[curr_state] + new_state[new_index]  # Calculating the cost to reach the new state.
+
+                    if new_state not in cost_so_far or new_cost < cost_so_far[new_state]:  # If the new state has not been visited before or the new cost is less than the previous cost of the state.
+                        cost_so_far[new_state] = new_cost  # Update the cost of the new state.
+                        priority = new_cost  # Set the priority of the new state to be the cost.
+                        heapq.heappush(heap, (priority, new_state))  # Add the new state to the priority queue with its priority. 
+
+                else:
+                    continue
+            else:
+                continue
+
+    return "Unsolvable"  # If the goal state is not reached, return "Unsolvable".```
+
+#The code uses a priority queue to explore the state space of the 8-puzzle problem in the order of increasing cost to reach each state. 
+#The algorithm maintains a dictionary `cost_so_far` to store the cost of each state. 
+#The cost of each state is the sum of the costs of the tiles in the state. 
+#The algorithm terminates when it reaches the goal state or when it has explored
+
+
+
+initial = [7, 2, 4, 
+           5, 0, 6, 
+           8, 3, 1]
+
+goal1 = [1, 3, 4, 
+         8, 0 ,2, 
+         7, 6, 5]
+
+goal2 = [1, 3, 4, 
+         8, 0, 6, 
+         7, 5, 2]
 
 print(f"test 1: {Dijkstra_Search(initial, goal1)}\nShould be 11\n")
 print(f"test 2: {Dijkstra_Search(initial, goal2)}\nShould be 30")
